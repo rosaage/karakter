032ded78f2292085d26b52293a95a6cb76b8f56b
Tage <Tage@192.168.10.130>
2014-10-27 20:00:04 +0100

    Hallo

---
+++ src/main/java/com/rosaage/karakter/App.java
@@ -0,0 +1,14 @@
+package com.rosaage.karakter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String Hello = "Hello World!";
+        System.out.println(Hello);
+    }
+}
